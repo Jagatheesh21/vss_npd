8f07b41bd99300b8218245fc94be2ccc67b2180e
Jagatheesh21 <jagatheesh21@gmail.com>
2023-04-08 12:44:08 +0530

    SampleSubmission
    Signed-off-by: Jagatheesh21 <jagatheesh21@gmail.com>

--- app/Http/Controllers/SampleSubmissionController.php
+++ app/Http/Controllers/SampleSubmissionController.php
@@ -57,6 +57,7 @@ class SampleSubmissionController extends Controller
      */
     public function store(StoreSampleSubmissionRequest $request)
     {
+        DB::beginTransaction();
         try {
 
             $quote = new SampleSubmission;
@@ -87,20 +88,21 @@ class SampleSubmissionController extends Controller
             $plan->current_sub_stage_id = 18;
             $plan->update();
             // Update Activity
-            $plan_activity->status_id = 4;
+            $plan_activity->status_id = 2;
             $plan_activity->actual_start_date = date('Y-m-d');
-            $plan_activity->actual_end_date = date('Y-m-d');
-            $plan_activity->gyr_status = 'G';
+            $plan_activity->prepared_at = Carbon::now();
             $plan_activity->update();
             $activity = APQPPlanActivity::find($plan->id);
             $user_email = auth()->user()->email;
             $user_name = auth()->user()->name;
             // Mail Function
-            Mail::to('edp@venkateswarasteels.com')->send(new ActivityMail($user_email,$user_name,$activity));
+            Mail::to('r.naveen@venkateswarasteels.com')->send(new ActivityMail($user_email,$user_name,$activity));
+            DB::commit();
             return back()->withSuccess('SIR Sample Submission Created Successfully!');
 
         } catch (\Throwable $th) {
             //throw $th;
+            DB::rollback();
             return back()->withErrors($th->getMessage());
         }
 
