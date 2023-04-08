5c98bb71891e4e23d8bf30610de47b3d4f4d677d
Jagatheesh21 <jagatheesh21@gmail.com>
2023-04-08 13:29:59 +0530

    ppap uptodate
    Signed-off-by: Jagatheesh21 <jagatheesh21@gmail.com>

--- app/Http/Controllers/PpapPreparationController.php
+++ app/Http/Controllers/PpapPreparationController.php
@@ -57,19 +57,20 @@ class PpapPreparationController extends Controller
      */
     public function store(StorePpapPreparationRequest $request)
     {
+       DB::beginTransaction();
         try {
 
             $quote = new PpapPreparation;
             $quote->apqp_timing_plan_id = $request->apqp_timing_plan_id;
             $quote->stage_id = 3;
-            $quote->sub_stage_id = 27;
+            $quote->sub_stage_id = 28;
             $quote->part_number_id = $request->part_number_id;
             $quote->revision_number = $request->revision_number;
             $quote->revision_date = $request->revision_date;
             $quote->application = $request->application;
             $quote->customer_id = $request->customer_id;
             $quote->product_description = $request->product_description;
-            $plan_activity = APQPPlanActivity::where('apqp_timing_plan_id',$request->apqp_timing_plan_id)->where('stage_id',3)->where('sub_stage_id',27)->first();
+            $plan_activity = APQPPlanActivity::where('apqp_timing_plan_id',$request->apqp_timing_plan_id)->where('stage_id',3)->where('sub_stage_id',28)->first();
             $file = $request->file('file');
             $fileName = time().'_'.$file->getClientOriginalName();
             $location = $plan_activity->plan->apqp_timing_plan_number.'/ppap_preparation';
@@ -84,23 +85,24 @@ class PpapPreparationController extends Controller
             // Update Timing Plan Current Activity
             $plan = APQPTimingPlan::find($request->apqp_timing_plan_id);
             $plan->current_stage_id = 3;
-            $plan->current_sub_stage_id = 27;
+            $plan->current_sub_stage_id = 28;
             $plan->update();
             // Update Activity
-            $plan_activity->status_id = 4;
+            $plan_activity->status_id = 2;
             $plan_activity->actual_start_date = date('Y-m-d');
-            $plan_activity->actual_end_date = date('Y-m-d');
-            $plan_activity->gyr_status = 'G';
+            $plan_activity->prepared_at = Carbon::now();
             $plan_activity->update();
             $activity = APQPPlanActivity::find($plan->id);
             $user_email = auth()->user()->email;
             $user_name = auth()->user()->name;
             // Mail Function
-            Mail::to('edp@venkateswarasteels.com')->send(new ActivityMail($user_email,$user_name,$activity));
+            Mail::to('r.naveen@venkateswarasteels.com')->send(new ActivityMail($user_email,$user_name,$activity));
+            DB::commit();
             return back()->withSuccess('PPAP Preparation Created Successfully!');
 
         } catch (\Throwable $th) {
             //throw $th;
+            DB::rollback();
             return back()->withErrors($th->getMessage());
         }
 
