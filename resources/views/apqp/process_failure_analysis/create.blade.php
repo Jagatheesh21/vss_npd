e707b74e5d2f62df53eec3eb690e5304524f4748
Jagatheesh21 <jagatheesh21@gmail.com>
2023-04-08 12:16:13 +0530

    PFEMA uptodate
    Signed-off-by: Jagatheesh21 <jagatheesh21@gmail.com>

--- resources/views/apqp/process_failure_analysis/create.blade.php
+++ resources/views/apqp/process_failure_analysis/create.blade.php
@@ -29,7 +29,7 @@
                     <div class="row mb-3">
                         <div class="col-md-3">
                             <label for="name" class="col-sm-6 col-form-label required">Timing Plan#</label>
-                            <select name="apqp_timing_plan_id" id="apqp_timing_plan_id" class="form-control select2">
+                            <select name="apqp_timing_plan_id" id="apqp_timing_plan_id" class="form-control select2 bg-light">
                                 @foreach ($plans as $t_plan)
                                     @if ($t_plan->id==$plan->id)
                                     <option value="{{$t_plan->id}}" selected>{{$t_plan->apqp_timing_plan_number}}</option>
@@ -42,7 +42,7 @@
                         </div>
                         <div class="col-md-3">
                             <label for="" class="col-sm-6 col-form-label required">Part Number*</label>
-                            <select name="part_number_id" id="part_number_id" class="form-control select2">
+                            <select name="part_number_id" id="part_number_id" class="form-control select2 bg-light">
                                 @foreach ($part_numbers as $part_number)
                                     @if ($part_number->id==$plan->part_number_id)
                                     <option value="{{$part_number->id}}" selected>{{$part_number->name}}</option>
@@ -55,21 +55,21 @@
                         </div>
                         <div class="col-md-3">
                             <label for="" class="col-sm-6 col-form-label required">Revision Number*</label>
-                            <input type="text" name="revision_number" class="form-control" readonly value="{{$plan->revision_number}}">
+                            <input type="text" name="revision_number" class="form-control bg-light" readonly value="{{$plan->revision_number}}">
                             @error('revision_number')
                             <span class="text-danger">{{$message}}</span>
                             @enderror
                         </div>
                         <div class="col-md-3">
                             <label for="" class="col-sm-6 col-form-label required">Revision Date*</label>
-                            <input type="text" name="revision_date" class="form-control" readonly  value="{{$plan->revision_date}}">
+                            <input type="text" name="revision_date" class="form-control bg-light" readonly  value="{{$plan->revision_date}}">
                             @error('revision_date')
                             <span class="text-danger">{{$message}}</span>
                             @enderror
                         </div>
                         <div class="col-md-3">
                             <label for="" class="col-sm-6 col-form-label required">Customer Type*</label>
-                            <select name="application" id="application" class="form-control select2">
+                            <select name="application" id="application" class="form-control select2 bg-light">
                                 @foreach ($customer_types as $customer_type)
                                     @if ($customer_type->id==$plan->customer->customer_type->id)
                                     <option value="{{$customer_type->id}}" selected>{{$customer_type->name}}</option>
@@ -83,7 +83,7 @@
 
                         <div class="col-md-3">
                             <label for="" class="col-sm-6 col-form-label required">Customer*</label>
-                            <select name="customer_id" id="customer_id" class="form-control select2">
+                            <select name="customer_id" id="customer_id" class="form-control select2 bg-light">
                                 @foreach ($customers as $customer)
                                     @if ($customer->id==$plan->customer_id)
                                     <option value="{{$customer->id}}" selected>{{$customer->name}}</option>
@@ -96,7 +96,7 @@
                         </div>
                         <div class="col-md-3">
                             <label for="" class="col-sm-8 col-form-label required">Product Description*</label>
-                            <select name="product_description" id="product_description" class="form-control select2">
+                            <select name="product_description" id="product_description" class="form-control select2 bg-light">
                                 @foreach ($part_numbers as $part_number)
                                     @if ($part_number->id==$plan->part_number_id)
                                     <option value="{{$part_number->id}}" selected>{{$part_number->description}}</option>
@@ -244,7 +244,6 @@
 
 @endsection
 @push('scripts')
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
 <script src="{{asset('js/select2.min.js')}}"></script>
 <script>
     $("#team").select2({
@@ -253,6 +252,7 @@
 });
 $("#submit").click(function(e){
         e.preventDefault();
+        var url = "{{route('activity.index')}}";
         $.ajax({
             url:"{{route('process_failure_analysis.store')}}",
             type:"POST",
@@ -266,11 +266,12 @@ $("#submit").click(function(e){
                   position: 'top-right',
                   icon: 'success'
               });
-             // location.reload();
+             window.location.href=url;
             },
             error:function(response)
             {
                 var result = $.parseJSON(response.responseText);
+
                 $.each(result.errors, function(key, val) {
                 $.toast({
                     heading: 'Error',
